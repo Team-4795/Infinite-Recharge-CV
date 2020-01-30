1d1f69d8222f5c3a751f1433e64368893112d92c
Peter <email>
2020-01-30 18:08:01 -0500

    finds red goal

---
+++ 360noscope.py
@@ -0,0 +1,83 @@
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import cv2
+import imutils
+import time
+from os import listdir
+from os.path import isfile, join
+import json
+from networktables import NetworkTables
+
+yellowLower = (0, 25, 220)
+yellowUpper = (10, 255, 255)
+
+vs = VideoStream(src=0).start()
+
+time.sleep(2.0)
+
+files = [f for f in listdir("./images") if isfile(join('./images', f))]
+
+NetworkTables.initialize(server='roborio-4795-frc.local')
+sd = NetworkTables.getTable('SmartDashboard')
+
+for file in files:
+    #frame = vs.read()
+    frame = cv2.imread('./images/' + file)#frame[1] if args.get("video", False) else frame
+    data = False
+    with open('./labels/' + file + '.json', "r") as read_file:
+        data = json.load(read_file)
+
+    if len(data['objects']) != 0:
+        continue
+
+    if frame is None:
+        break
+
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+    mask = cv2.dilate(mask, None, iterations=3)
+    
+    ''''mask = cv2.erode(mask, None, iterations=3)
+    mask = cv2.dilate(mask, None, iterations=7)
+    mask = cv2.erode(mask, None, iterations=2)'''
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    width = frame.shape[1]
+    height = frame.shape[0]
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+            sd.putNumber('ball_x', int(x - width / 2))
+            sd.putNumber('ball_y', int(y - height / 2))
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("x", mask)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("v"):
+        time.sleep(15)
+
+    time.sleep(2)
+
+# if we are not using a video file, stop the camera video stream
+if not args.get("video", False):
+    vs.stop()
+
+# otherwise, release the camera
+else:
+    vs.release()
+
+cv2.destroyAllWindows()
--- ball_tracking.py
+++ ball_tracking.py
@@ -1,101 +1,74 @@
-# USAGE
-# python ball_tracking.py --video ball_tracking_example.mp4
-# python ball_tracking.py
-
-# import the necessary packages
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
 import imutils
 import time
+from os import listdir
+from os.path import isfile, join
+import json
+from networktables import NetworkTables
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-	help="max buffer size")
-args = vars(ap.parse_args())
+yellowLower = (15, 160, 60)
+yellowUpper = (30, 255, 255)
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (15, 180, 140)
-greenUpper = (30, 255, 255)
+vs = VideoStream(src=0).start()
 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()
-
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-
-# allow the camera or video file to warm up
 time.sleep(2.0)
 
-# keep looping
-while True:
-	# grab the current frame
+files = [f for f in listdir("./images") if isfile(join('./images', f))]
+
+NetworkTables.initialize(server='roborio-4795-frc.local')
+sd = NetworkTables.getTable('SmartDashboard')
+
+while True:#for file in files:
 	frame = vs.read()
+	'''frame = cv2.imread('./images/' + file)#frame[1] if args.get("video", False) else frame
+	data = False
+	with open('./labels/' + file + '.json', "r") as read_file:
+		data = json.load(read_file)
 
-	# handle the frame from VideoCapture or VideoStream
-	frame = frame[1] if args.get("video", False) else frame
+	if len(data['objects']) == 0:
+		continue'''
 
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
 	if frame is None:
 		break
 
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
 	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (15, 15), 0)
+	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+	mask = cv2.erode(mask, None, iterations=3)
+	mask = cv2.dilate(mask, None, iterations=7)
 	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
 
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	center = None
+	width = frame.shape[1]
+	height = frame.shape[0]
 
-	# only proceed if at least one contour was found
 	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
 		c = max(cnts, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		# only proceed if the radius meets a minimum size
-		if radius > 5:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
+		if radius > 10:
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+			sd.putNumber('ball_x', int(x - width / 2))
+			sd.putNumber('ball_y', int(y - height / 2))
 
-	# show the frame to our screen
 	cv2.imshow("Frame", frame)
-	cv2.imshow("x", mask)
+	#cv2.imshow("x", mask)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+	if key == ord("v"):
+		time.sleep(15)
+
+	#time.sleep(1)
 
 # if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
@@ -105,5 +78,4 @@ if not args.get("video", False):
 else:
 	vs.release()
 
-# close all windows
 cv2.destroyAllWindows()
